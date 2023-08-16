bde3c76b16e13cd8407938567274fc2ef018b970
Sadık Demir <demirsadik1905@gmail.com>
2023-08-16 23:33:39 +0300

    Context Class

---
+++ MVC_CommercialAutomation/Models/Classes/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVC_CommercialAutomation.Models.Classes
+{
+    public class Context: DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Current> Currents { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SalesMovement> SalesMovements { get; set; }
+
+    }
+}
